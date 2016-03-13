e87f2a3fb014a751e1f58db9fdf472987df0b951
Lighting <Lighting@users.noreply.github.com>
2016-03-14 01:34:33 +0300

    Update xrwsunpack.c

--- xrwsunpack.c
+++ xrwsunpack.c
@@ -65,27 +65,27 @@ void unpack(const char *file, const char *out_dir)
 		terminate("No access to directory %s", out_dir);
 
 	//parse name of input file
-printf("file %s", file);
+printf("file %s\n", file);
 	name_start = strrchr(file, '/') + 1;
-printf("name_start %s", name_start);
+printf("name_start %s\n", name_start);
 	name_end = strstr(name_start, PARSE_EXTENSIONS);
-printf("name_end %s", name_end);
+printf("name_end %s\n", name_end);
 	if(name_end != name_start)
 		terminate("Name of file %s must begin with \"%s\"%s", file, PARSE_EXTENSIONS, PARSE_WARNING);
 	name_start += sizeof(PARSE_EXTENSIONS);
-printf("name_start %s", name_start);
+printf("name_start %s\n", name_start);
 	name_end = strrchr(name_start, '.');
-printf("name_end %s", name_end);
+printf("name_end %s\n", name_end);
 	if(name_end == NULL || strcmp(name_end, PARSE_DAT) != 0)
 		terminate("Extension of file %s must be \"%s\"%s", file, PARSE_DAT, PARSE_WARNING);
 	strncpy(out_path, name_start, name_end - name_start);
-printf("out_path %s", out_path);
+printf("out_path %s\n", out_path);
 	name_end = strrchr(out_path, PARSE_VERSION_TOKEN);
-printf("name_end %s", name_end);
+printf("name_end %s\n", name_end);
 	if(name_end != NULL && name_end[1] == PARSE_VERSION)
 		terminate("Name of file %s must contain version number%s", file, PARSE_WARNING);
 	strncpy(out_path2, out_path, name_end - out_path);
-printf("out_path2 %s", out_path2);
+printf("out_path2 %s\n", out_path2);
 
 	//open XRWS file for reading
 	ifd = fopen(file, "rb");
