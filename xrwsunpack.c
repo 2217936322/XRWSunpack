7b13a7ee0e84802a0d46bcc903718cffcf6ec990
Lighting <Lighting@users.noreply.github.com>
2016-03-13 15:42:30 +0300

    Create xrwsunpack.c

---
+++ xrwsunpack.c
@@ -0,0 +1,92 @@
+/*
+ XRWSunpack - Unpack X Rebirth Workshop .dat files downloaded from Steam
+ Developed by Lit (https://github.com/Lighting/XRWSunpack)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+#define MAXSIZE 520000
+#define XRWS_SIGNATURE "XRWS"
+#define XRWS_VERSION 1
+
+const char *xrwsunpack_header = "XRWSunpack v0.1 (" __DATE__ " " __TIME__ ")";
+
+void terminate(const char *fmt, ...)
+{
+	va_list ap;
+
+	fprintf(stderr, "Error: ");
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
+void usage(char **argv)
+{
+	fprintf(stderr, "%s\n", xrwsunpack_header);
+	fprintf(stderr, "Usage: %s [option] .dat_file [OUT_DIR]\n", argv[0]);
+	fprintf(stderr, "Unpack X Rebirth Workshop (XRWS) .dat files downloaded from Steam\n\n");
+	fprintf(stderr, "Report bugs to <https://github.com/Lighting/XRWSunpack/issues>\n");
+}
+
+void unpack(const char *file, const char *out_dir)
+{
+	FILE *ifd, *ofd;
+	struct header_struct {
+    char sig[4];
+    unsigned long ver;
+    unsigned long files_numder;
+    unsigned long files_names_len;
+    unsigned long files_size;
+	} header;
+	unsigned long *files_sizes;
+	char *files_names, *data;
+	
+	//open XRWS file for reading
+	ifd = fopen(file, "rb");
+	if(ifd == NULL)
+		terminate("Cannot open theme file %s", file);
+	
+	//read and check header
+	fread(header, 1, sizeof(header), ifd);
+	if(strncmp(header.sig, XRWS_SIGNATURE, 4) != 0)
+		terminate("%s is not a XRWS file", file);
+	if(header.ver != XRWS_VERSION)
+		terminate("%s have unsupported XRWS version %d", file, header.ver);
+	
+	//read sizes of files
+	files_sizes = malloc(header.files_number * 4);
+	memset(files_sizes, 0, sizeof(files_sizes));
+	fread(files_sizes, 1, sizeof(files_sizes), ifd);
+  
+  //read names of files
+	files_names = malloc(header.filename_len);
+	memset(files_names, 0, sizeof(files_names));
+	fread(files_names, 1, sizeof(files_names), ifd);
+	
+	fclose(ifd);
+	
+	free(files_sizes);
+	free(files_names);
+}
+
+int main(int argc, char **argv)
+{
+	if(argc < 2)
+	{
+		usage(argv);
+		terminate("\nTheme file not found");
+	}
+	
+	if(strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
+    usage(argv);
+	else
+		unpack(argv[1], (argc > 2) ? argv[2] : "-");
+
+	return 0;
+}
